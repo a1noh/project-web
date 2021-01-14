3c3e04e567fa87bf66b76eb075ed740d0eb8149b
austin noh <noh1austin@gmail.com>
2021-01-14 21:10:42 +0900

    added components

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,17 @@
+import React from 'react'
+// const About = () => {
+//     var s = "Austin"
+//     return(
+//         <h1>Hello. {s}</h1>
+//     )
+// }
 
-
-
-const About = () => {
-    var s = "Austin"
-    return(
-        <h1>Hello. {s}</h1>
-    )
+class About extends React.Component {
+    render(){
+        return (
+            <h1>hello I am class componenet</h1>
+        )
+    }
 }
 
 
