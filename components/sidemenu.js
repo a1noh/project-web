b97ef00c5e2c78d3359ba71486a4f00b65151b47
austin noh <noh1austin@gmail.com>
2021-01-16 21:31:12 +0900

    form

--- components/sidemenu.js
+++ components/sidemenu.js
@@ -1,17 +1,22 @@
+import { useState } from "react";
+import Modal from "./modal";
+import MovieCreateForm from "./movieCreateForm";
+
+// Containment
 const SideMenu = (props) => {
+  const { categories } = props;
   return (
     <div>
+      <Modal>
+        <MovieCreateForm />
+      </Modal>
       <h1 className="my-4">{props.appName}</h1>
       <div className="list-group">
-        <a href="#" className="list-group-item">
-          Category 1
-        </a>
-        <a href="#" className="list-group-item">
-          Category 2
-        </a>
-        <a href="#" className="list-group-item">
-          Category 3
-        </a>
+        {categories.map((c) => (
+          <a key={c.id} href="#" className="list-group-item">
+            {c.name}
+          </a>
+        ))}
       </div>
     </div>
   );
